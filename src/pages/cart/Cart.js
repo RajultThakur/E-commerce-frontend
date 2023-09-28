1a7945a73bcc4201958b3597618fc4c2b3bc3a33
Rajul Thakur <thakurajul8888@gmail.com>
2023-09-28 12:47:30 +0530

    updated cart functionality

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Utils from '../../utils/helper';
 import CartItem from './CartItem';
 import Checkout from './Checkout';
 import "../admin/admin.css"
+import "../../components/adminAccount/sidebar.css"
+import { AUTH_TOKEN } from '../../constants/constants';
+import NotLoggedIn from '../../components/error/NotLoggedIn';
+import Loading from '../../components/Loading';
+import NotFound from '../../components/utils/NotFound';
 export default function Cart () {
     const [context] = Utils();
-    const { cartItems, cartPrice } = context;
+    const { cartItems, cartPrice, loggedUser, getCartItems, getUserByToken } = context;
+    const [loading, setLoading] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(true);
+
+    useEffect(() => {
+        async function run () {
+            setLoading(true)
+            const user = await getUserByToken();
+            await getCartItems(user.id);
+            console.log(cartItems)
+            setLoading(false)
+
+        }
+        if (AUTH_TOKEN === null) {
+
+            setLoggedIn(false)
+        }
+        else {
+            run();
+        }
+    }, [])
+
     return (
         <div className='flex gap-2 customHeight'>
-            <div className='flex-1 p-5 overflow-y-scroll '>
+            {loggedIn == false ? <NotLoggedIn /> : loading == true ? <Loading /> : cartItems.length != 0 ? <>
+                <div className='flex-1 p-5 overflow-y-scroll productPage '>
 
-                {cartItems.map((ele) => {
-                    return (
-                        <CartItem key={ele._id} product={ele.product} quantity={ele.quantity} />
-                    )
-                })}
-            </div>
-            <div className='customHeight w-[250px] h-[170px] p-1 bg-red-300'>
-                <Checkout total={cartPrice} cartItems={cartItems} />
-            </div>
-        </div>
+                    {cartItems.map((ele) => {
+                        return (
+                            <CartItem key={ele._id} product={ele.product} id={ele._id} quantity={ele.quantity} />
+                        )
+                    })}
+                </div>
+                <div className='customHeight w-[350px] h-[170px] p-1'>
+                    <Checkout total={cartPrice} cartItems={cartItems} />
+                </div></> :
+                <NotFound heading="Your cart is empty"
+                    subHeading="Add more items in you cart form." from="here" />
+            }
+        </div >
     )
 }
--- src/pages/cart/CartItem.js
+++ src/pages/cart/CartItem.js
@@ -1,39 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Utils from '../../utils/helper';
 import './cart.css'
-export default function CartItem ({ product, quantity }) {
+import { toast } from 'react-toastify';
+import { AUTH_TOKEN } from '../../constants/constants';
+
+export default function CartItem ({ product, id, quantity }) {
     const { title, img, brand, price } = product;
+    const [context] = Utils();
+    const { removeFromCart, getCartItems, loggedUser } = context;
+    const [loggedIn, setLoggedIn] = useState(true);
+    const [selectedOption, setSelectedOption] = useState('option1');
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    const remove = async (id) => {
+        const data = await removeFromCart(id);
+        if (data.success) {
+            await getCartItems(loggedUser.id);
+            toast.success(data.message, {
+                position: toast.POSITION.TOP_CENTER
+            })
+        } else {
+            toast.error(data.message, {
+                position: toast.POSITION.TOP_CENTER
+            })
+        }
+    }
+
+    // useEffect(() => {
+    //     if (AUTH_TOKEN === null) {
+    //         setLoggedIn(false)
+    //     }
+    // }, [])
+
+
     return (
 
         <div className='flex items-center justify-around'>
             <div className='w-[130px] h-[150px]'>
-                <img className='object-cover w-[100%] h-[100%]' src={img} alt="" />
+                <img className='object-contain w-[100%] h-[100%]' src={img} alt="" />
             </div>
-            <div className='flex flex-col gap-3'>
+            <div className='flex flex-col item gap-3'>
                 <div>
-                    <h1 className='text-xl  font-medium'>{title}</h1>
+                    <h1 className='text-xl  font-medium'>{title.substring(0, 20)}</h1>
                 </div>
-                <div>
-                    <div className='flex gap-2 justify-around items-center'>
+                <div className='flex gap-2 justify-around items-center'>
+                    <div >
                         <h1 className='text-md'>Price  <span className='font-medium ml-2'>₹{price}/</span></h1>
-                        <div>
-                            <h1 className='text-md italic'>powered by  <span className='font-medium italic text-gray-400 underline'>{brand}</span></h1>
-                        </div>
+                    </div>
+                    <div>
+                        <h1 className='text-md italic'>powered by  <span className='font-medium italic text-gray-400 underline'>{brand}</span></h1>
                     </div>
                 </div>
                 <div className='flex items-center justify-between'>
-                    <div className='flex quantityBtn'>
-                        <div className=''>
-                            <button className=''>-</button>
-                        </div>
-                        <div>
-                            {quantity}
-                        </div>
-                        <div>
-                            <button>+</button>
-                        </div>
-                    </div>
+
+                    <select className='border border-[#d4cccc] rounded-md px-2 py-1 outline-none' value={selectedOption} onChange={handleOptionChange} >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
                     <div>
-                        <button className='bg-red-400 outline-none rounded-md py-[6px] font-medium px-4 hover:bg-red-400 text-white'>Buy now</button>
+                        <button className='bg-red-400 outline-none rounded-md py-[6px] font-medium px-4 hover:bg-red-400 text-white'
+                            onClick={() => { remove(id) }}>Remove</button>
                     </div>
                 </div>
 

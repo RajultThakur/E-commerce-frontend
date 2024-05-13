ffbcce028199a561a3786e1580e13cf6550df93a
Rajul Thakur <thakurajul8888@gmail.com>
2024-05-13 16:31:49 +0530

    added cart Slick in store

---
+++ src/store/cartSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import config from "../config/config";
+
+const initialState = {
+    cartProducts: [],
+    isLoading : false,
+    isError : false
+}
+
+export const fetchCartProducts = createAsyncThunk("fetchCartProducts", async (userId) => {
+    const response = await fetch(`${config.backendEndPoint}/cart/cart-items/${userId}`);
+    const {data} = await response.json();
+    console.log(data);
+    return data;
+})
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    extraReducers : (builder) => {
+        builder.addCase(fetchCartProducts.pending, (state) => {
+            state.isLoading = true;
+        })
+        builder.addCase(fetchCartProducts.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartProducts = action.payload
+        })
+        builder.addCase(fetchCartProducts.rejected, (state) => {
+            state.isError = true;
+        })
+    },
+    reducers: {
+
+    }
+})
+
+// export const {} = cartSlice.actions;
+export default cartSlice.reducer
--- src/store/combineReducers.js
+++ src/store/combineReducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import productSlice from "./productSlice"
+import cartSlice from "./cartSlice";
 
 const rootReducers = combineReducers({
     auth : authSlice,
-    products  : productSlice
+    products  : productSlice,
+    cartProducts : cartSlice
 })
 
 export default rootReducers
